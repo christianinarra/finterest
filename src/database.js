aef2dae6b1398a7228eb2ef5110b627bb783b8f8
Christian <christian.inarra@swissbytes.ch>
2021-04-01 19:29:57 -0400

    change password mongoDB

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-///mongoose.connect('mongodb://localhost/finterest', {
-mongoose.connect('mongodb+srv://christiandev:Drux217814%@cluster0.vev98.mongodb.net/finterest-db?retryWrites=true&w=majority', {
+//mongoose.connect('mongodb://localhost/finterest', {
+mongoose.connect('mongodb+srv://christiandev:L4lQwRVMuwwbs3mB@cluster0.vev98.mongodb.net/finterest-db?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
     .then(db => console.log('Db is connected'))
